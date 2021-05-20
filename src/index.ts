a6dbdd11a5ff10ad6fc9f38969243951abfe70d8
Marcel Blijleven <marcelblijleven@gmail.com>
2021-05-20 10:50:20 +0200

    refactor: move ms teams service to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import type { Services} from "@wdio/types";
-import type { Capabilities} from "@wdio/types/build/Capabilities";
-import type { Testrunner as TestRunnerOptions } from "@wdio/types/build/Options";
-import {Test, TestResult} from "@wdio/types/build/Frameworks";
+import MsTeamsService from "./ms-teams-service";
 
-export default class MsTeamsService implements Services.ServiceInstance {
-  private _failedTests: number
-  private _passedTests: number
-  private _testsCount: number
-
-  constructor(serviceOptions: Services.ServiceOption, capabilities: Capabilities, config: TestRunnerOptions) {
-    this._failedTests = 0;
-    this._passedTests = 0;
-    this._testsCount = 0;
-  }
-
-  async afterTest(test: Test, context: any, result: TestResult) {
-    this._passedTests += 1;
-  }
-}
+export { MsTeamsService };
---
+++ src/ms-teams-service.ts
@@ -0,0 +1,20 @@
+import type { Services } from "@wdio/types";
+import type { Capabilities } from "@wdio/types/build/Capabilities";
+import type { Testrunner as TestRunnerOptions } from "@wdio/types/build/Options";
+import { Test, TestResult } from "@wdio/types/build/Frameworks";
+
+export default class MsTeamsService implements Services.ServiceInstance {
+    private _failedTests: number;
+    private _passedTests: number;
+    private _testsCount: number;
+
+    constructor(serviceOptions: Services.ServiceOption, capabilities: Capabilities, config: TestRunnerOptions) {
+        this._failedTests = 0;
+        this._passedTests = 0;
+        this._testsCount = 0;
+    }
+
+    async afterTest(test: Test, context: any, result: TestResult) {
+        this._passedTests += 1;
+    }
+}
