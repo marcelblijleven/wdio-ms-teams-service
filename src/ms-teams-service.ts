5e88dcec598a8d777c9bd9a3e9fa3cbcd74123fb
Marcel Blijleven <marcelblijleven@gmail.com>
2021-05-21 16:50:25 +0200

    refactor: make service use test result container

--- src/ms-teams-service.ts
+++ src/ms-teams-service.ts
@@ -4,12 +4,12 @@ import type { Testrunner as TestRunnerOptions } from "@wdio/types/build/Options"
 import { Test, TestResult } from "@wdio/types/build/Frameworks";
 import { Browser, MultiRemoteBrowser } from "webdriverio";
 import IncomingWebhook from "./incoming-webhook";
-import { ITestResult } from "./types";
 import { AdaptiveCard } from "./adaptive-card";
+import TestResultContainer, { ITestResult } from "./test-result-container";
 
 export default class MsTeamsService implements Services.ServiceInstance {
+    readonly testResultContainer: TestResultContainer;
     private _webhook: IncomingWebhook;
-    private readonly _testResults: Array<ITestResult>;
     private _browser?: Browser<"async"> | MultiRemoteBrowser<"async">;
     private _capabilities: Capabilities;
     private _config: TestRunnerOptions;
@@ -18,7 +18,7 @@ export default class MsTeamsService implements Services.ServiceInstance {
         this._webhook = new IncomingWebhook(serviceOptions.webhookURL);
         this._capabilities = capabilities;
         this._config = config;
-        this._testResults = [];
+        this.testResultContainer = new TestResultContainer();
     }
 
     before(
@@ -30,19 +30,20 @@ export default class MsTeamsService implements Services.ServiceInstance {
     }
 
     async afterTest(test: Test, context: any, result: TestResult): Promise<void> {
+        const testName = test.parent || test.fullName;
         const testResult: ITestResult = {
             passed: result.passed,
-            error: result.error,
+            error: result.error || "",
             title: test.title,
             description: test.description,
         };
 
-        this._testResults.push(testResult);
+        this.testResultContainer.addTest(testName, testResult);
     }
 
     async after(): Promise<void> {
         const message = "An automated test run just completed";
-        const adaptiveCard = new AdaptiveCard(message, this._testResults);
+        const adaptiveCard = new AdaptiveCard(message, this.testResultContainer);
         await this._webhook.send(adaptiveCard.toString());
     }
 }
